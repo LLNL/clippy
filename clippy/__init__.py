ec840f9ce9ebcacc274cfbb01318a4c4f98b5a03
Seth Bromberger <github@bromberger.com>
2021-03-06 08:36:29 -0500

    first push

---
+++ clippy/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2020 Lawrence Livermore National Security, LLC and other CLIPPy Project Developers.
+# See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from clippy.clippy import Clippy
