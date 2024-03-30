9afbb406b9215bf7bba2d6711b7f67b2f464ebd3
Seth Bromberger <github@bromberger.com>
2024-03-30 12:07:58 -0700

    lint

--- src/clippy/backends/fs/execution.py
+++ src/clippy/backends/fs/execution.py
@@ -8,7 +8,7 @@ import logging
 import subprocess
 from ...clippy_types import AnyDict
 from ... import cfg
-from ...constants import OUTPUT_KEY, RETURN_KEY
+from ...constants import OUTPUT_KEY
 from .constants import (
     DRY_RUN_FLAG,
     HELP_FLAG,
